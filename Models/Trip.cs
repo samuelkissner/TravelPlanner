4c14f7eb0a4ff0e3d384a05e6c2bac7802137dd0
samuelkissner <samuelkissner@gmail.com>
2021-08-28 11:32:21 -0500

    - made certain non-nullable types in trip model nullable - added entity framework core tools to dependencies

--- Models/Trip.cs
+++ Models/Trip.cs
@@ -8,16 +8,13 @@ namespace TravelPlanner.Models
 {
     public class Trip
     {
-        public int ID { get; set; }
-        
+        public int ID { get; set; }   
         [Required]
         public string Name { get; set; }
-        
-        
         public string Description { get; set; }
-        public DateTime DepartureDate { get; set; }
-        public DateTime ReturnDate { get; set; }
-        public double Budget { get; set; }
+        public DateTime? DepartureDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public double? Budget { get; set; }
         public string PhotoPath { get; set; }
     }
 }
