d1a1c3bb15d735adf39a064f01d5c96995ceac8d
samuelkissner <samuelkissner@gmail.com>
2021-08-30 15:04:44 -0500

    - registered DbContext class and configured it to use a local sql server db (which will be created code first using EntityFrameworkCore)

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -8,12 +8,14 @@ namespace TravelPlanner.Models
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Trip> Trips { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) 
             : base(options)
         {
 
         }
 
-        public DbSet<Trip> Trips { get; set; }
+        
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +25,8 @@ namespace TravelPlanner
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("TravelPlannerDBConection")));
             services.AddControllersWithViews();
             services.AddSingleton<ITripRepository, MockTripRepositoryImplementation>();
 
