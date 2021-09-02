d561895655a7760150366ec4a4c77d35a38dcc50
samuelkissner <samuelkissner@gmail.com>
2021-09-02 15:24:56 -0500

    - added seed data to the Trip table of the sql server database using the OnModelBuilding method of the DbContext class

---
+++ Migrations/20210902202035_SeedTripTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelPlanner.Migrations
+{
+    public partial class SeedTripTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Trips",
+                columns: new[] { "ID", "Budget", "DepartureDate", "Description", "Name", "PhotoPath", "ReturnDate" },
+                values: new object[] { 1, 5000.0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Tim is having a destination wedding in Findland at the end of the year.", "Tim's Wedding", null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Trips",
+                columns: new[] { "ID", "Budget", "DepartureDate", "Description", "Name", "PhotoPath", "ReturnDate" },
+                values: new object[] { 2, 600.0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Attending a halloween-themed costume party at the company branch in Austin.", "Halloween Party", null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Trips",
+                columns: new[] { "ID", "Budget", "DepartureDate", "Description", "Name", "PhotoPath", "ReturnDate" },
+                values: new object[] { 3, 2500.0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Two week, all-inclusive.", "Carribean Cruise", null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Trips",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Trips",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Trips",
+                keyColumn: "ID",
+                keyValue: 3);
+        }
+    }
+}
--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -16,6 +16,17 @@ namespace TravelPlanner.Models
 
         }
 
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Trip>().HasData(
+
+                new Trip() { ID = 1, Name = "Tim's Wedding", Description = "Tim is having a destination wedding in Findland at the end of the year.", DepartureDate = new DateTime(12 / 05 / 2021), ReturnDate = new DateTime(12 / 11 / 2021), Budget = 5000 },
+                new Trip() { ID = 2, Name = "Halloween Party", Description = "Attending a halloween-themed costume party at the company branch in Austin.", DepartureDate = new DateTime(10 / 28 / 2021), ReturnDate = new DateTime(11 / 01 / 2021), Budget = 600 },
+                new Trip() { ID = 3, Name = "Carribean Cruise", Description = "Two week, all-inclusive.", DepartureDate = new DateTime(07 / 01 / 2022), ReturnDate = new DateTime(07 / 14 / 2022), Budget = 2500 }
+
+
+
+                );
+        }
     }
 }
